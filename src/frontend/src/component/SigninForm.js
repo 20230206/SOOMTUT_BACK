c31ecbe61fd4487db3d25a608a8c35fe6fd4c9f7
lalyns <kelwho@naver.com>
2023-02-11 03:49:48 +0900

    #30 [Feat] 로그인 페이지 구성

--- src/frontend/src/component/SigninForm.js
+++ src/frontend/src/component/SigninForm.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 import styles from "../assets/styles/formstyle.module.css"
 
-function SignupForm() {
-    return (
+import { Link } from "react-router-dom"
+import kakao from "../assets/images/kakaosignup.png"
+
+function SigninForm() {
+       return (
         <div className={styles.wrapper}>
          <div className={styles.formbox}>
           <text className={styles.title} style={{color:"#4E57DE"}}> SOOMTUT </text>
           <br></br>
-          <text className={styles.title}>회원가입</text>
+          <text className={styles.title}>로그인</text>
           <br></br>
           <br></br>
            <Form>
@@ -20,26 +23,6 @@ function SignupForm() {
               placeholder="email"
               className={styles.input} />
              <br></br>
-             <Form.Text
-              className="text-muted"
-              style={{color:"red"}}>
-                 가입이 불가능한 이메일입니다.
-             </Form.Text>
-            </Form.Group>
-    
-            <Form.Group className={styles.Group}>
-             <Form.Label className={styles.label}>Nickname</Form.Label>
-             <br></br>
-             <Form.Control
-              type="password"
-              placeholder="nickname"
-              className={styles.input} />
-             <br></br>
-             <Form.Text
-              className="text-muted"
-              style={{color:"red"}} >
-                 사용이 불가능한 닉네임입니다.
-             </Form.Text>
             </Form.Group>
     
             <Form.Group className={styles.Group}>
@@ -53,12 +36,14 @@ function SignupForm() {
             </Form.Group>
                 
             <Button className={styles.summit} variant="primary" type="submit">
-            가입하기
+            로그인
             </Button>
            </Form>
+          
+          <Link to="/signupkakao"> <img src={kakao} className={styles.kakao} alt="kakaosignup" /> </Link>
          </div>
         </div>
-    );
+       );
 }
 
-export default SignupForm;
+export default SigninForm
