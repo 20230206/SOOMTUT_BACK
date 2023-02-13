587bf961a61f921cd81717817a86bd5af2916533
Shinny <siniya979@gmail.com>
2023-02-13 17:23:24 +0900

    #17[Feat]_나의등록수업조회
    유저가 본인이 등록한 수업 목록을 조회하는 기능 구현
    - 코드 리팩토링 필요 
    - 쿼리 성능 고민

---
+++ src/main/java/com/sparta/soomtut/dto/PostResponseDto.java
@@ -0,0 +1,20 @@
+package com.sparta.soomtut.dto;
+
+import com.sparta.soomtut.entity.Category;
+import com.sparta.soomtut.entity.Post;
+
+public class PostResponseDto {
+    private Category category;
+    // 임시
+    private String image;
+    private int fee;
+    private String tutorNickname;
+    private String location;
+    public PostResponseDto(Post post, String nickName, String location) {
+        this.category = post.getCategory();
+        this.image = "기본 이미지 주소";
+        this.fee = post.getFee();
+        this.tutorNickname = nickName ;
+        this.location = location;
+    }
+}
