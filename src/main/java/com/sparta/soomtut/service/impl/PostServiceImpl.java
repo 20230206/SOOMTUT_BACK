1ad87b5453dcbadf563b4103ec240044960f5fc6
LeeEunseop <xxxnseop@gmail.com>
2023-02-14 15:55:05 +0900

    #14 [feat] 게시글 생성, 수정, 삭제  추가기능 구현

--- src/main/java/com/sparta/soomtut/service/impl/PostServiceImpl.java
+++ src/main/java/com/sparta/soomtut/service/impl/PostServiceImpl.java
@@ -5,42 +5,58 @@ import com.sparta.soomtut.dto.request.UpdatePostRequestDto;
 import com.sparta.soomtut.dto.response.PostResponseDto;
 import com.sparta.soomtut.entity.Member;
 import com.sparta.soomtut.entity.Post;
+import com.sparta.soomtut.enums.MemberRole;
 import com.sparta.soomtut.exception.ErrorCode;
-import com.sparta.soomtut.repository.MemberRepository;
 import com.sparta.soomtut.repository.PostRepository;
+import com.sparta.soomtut.service.interfaces.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class PostServiceImpl {
+public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
 
 
     // 게시글 작성
-    public PostResponseDto createPost(String member, PostRequestDto postRequestDto) {
-        Post post = new Post(member, postRequestDto.getTitle(), postRequestDto.getImage(), postRequestDto.getContent(), postRequestDto.getFee());
+    @Transactional
+    public PostResponseDto createPost(Member member, PostRequestDto postRequestDto) {
+        Post post = new Post(postRequestDto, member);
         postRepository.save(post);
         return new PostResponseDto(post);
     }
 
     // 게시글 수정
-    public PostResponseDto updatePost(Long postId, UpdatePostRequestDto updatePostRequestDto) {
+    @Transactional
+    public PostResponseDto updatePost(Long postId, UpdatePostRequestDto updatePostRequestDto, Member member) {
         Post post = postRepository.findByPostId(postId).orElseThrow(
-                () -> new IllegalArgumentException(ErrorCode.EMPTY_POST.getMessage())
+                () -> new IllegalArgumentException(ErrorCode.NOT_FOUND_POST.getMessage())
         );
+
+        // 작성자 또는 관리자만 수정가능
+        if (member.getMemberRole() != MemberRole.ADMIN) {
+            if (!post.getMember().getId().equals(member.getId()))
+                throw new IllegalArgumentException(ErrorCode.AUTHORIZATION.getMessage());
+        }
+
         post.update(updatePostRequestDto);
         return new PostResponseDto(post);
     }
 
     //게시글 삭제
-    public void deletePost(Long postId) {
+    @Transactional
+    public void deletePost(Long postId, Member member) {
         Post post = postRepository.findByPostId(postId).orElseThrow(
-                () -> new IllegalArgumentException(ErrorCode.EMPTY_POST.getMessage())
+                () -> new IllegalArgumentException(ErrorCode.NOT_FOUND_POST.getMessage())
         );
+
+        if (member.getMemberRole() == MemberRole.ADMIN)
+            postRepository.delete(post);
+
         postRepository.deleteById(postId);
     }
 
