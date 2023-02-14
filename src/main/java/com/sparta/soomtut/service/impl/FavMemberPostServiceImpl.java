0152f435b8e1b33b6ef88b474d67b62c0fc3aea6
Lim DongGyu <ldg1160296@gmail.com>
2023-02-14 15:23:44 +0900

    [fix] 2차 피드백 => FavMemberPostServiceImpl 코드 수정

--- src/main/java/com/sparta/soomtut/service/impl/FavMemberPostServiceImpl.java
+++ src/main/java/com/sparta/soomtut/service/impl/FavMemberPostServiceImpl.java
@@ -7,6 +7,7 @@ import com.sparta.soomtut.entity.Post;
 import com.sparta.soomtut.exception.FavNotFoundException;
 import com.sparta.soomtut.repository.FavMemberPostRepository;
 import com.sparta.soomtut.service.interfaces.FavMemberPostService;
+import com.sparta.soomtut.service.interfaces.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FavMemberPostServiceImpl implements FavMemberPostService {
     private final FavMemberPostRepository favMemberPostRepository;
+    private final PostService postService;
 
     //즐겨찾기 업데이트
     @Transactional
+    @Override
     public String updateOfFavPost(FavPostDto favPostDto){
         Long Id = favPostDto.getId();
         Member member = favPostDto.getMember();
-        Post post = favPostDto.getPost();
+        Post post = postService.findPostById(favPostDto.getId()); //postSevice에 이미 구현된 기능 사용
 
         if(!hasFavPost(Id,member)){
             //메소드에 값을 가지고 있지 않다면 post에 favorit 카운트+1 그리고 favMemberPost에 저장
@@ -31,14 +34,15 @@ public class FavMemberPostServiceImpl implements FavMemberPostService {
         post.decreFavCount();
         return removeFavPost(post, member);
     }
-
     //즐겨찾기 추가
+    @Transactional
     public String createFavPost(Post post, Member member){
         FavMemberPost favMemberPost = new FavMemberPost(post, member);
         favMemberPostRepository.save(favMemberPost);
         return "SUCCESS_FavPost";
     }
     //즐겨찾기 취소
+    @Transactional
     public String removeFavPost(Post post, Member member){
         FavMemberPost favMemberPost = favMemberPostRepository.findByPostAndMember(post,member)
                 .orElseThrow(FavNotFoundException::new);
