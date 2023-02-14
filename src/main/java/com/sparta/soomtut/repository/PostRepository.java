7d31a299bb71d7e1ac9bcd3cfaefb365c1987336
LeeEunseop <xxxnseop@gmail.com>
2023-02-14 16:07:45 +0900

    #14 파일추가

---
+++ src/main/java/com/sparta/soomtut/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.soomtut.repository;
+
+import com.sparta.soomtut.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository <Post, Long> {
+    Optional<Post>findByPostId(Long postId);
+
+
+}
