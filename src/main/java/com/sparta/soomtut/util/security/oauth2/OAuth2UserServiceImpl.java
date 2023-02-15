512c91ada6c94e8e2813ce6008387a20ef152edb
lalyns <kelwho@naver.com>
2023-02-15 11:52:23 +0900

    #13 [Feat] 로그인 후처리 (토큰 발급 후 front에 전달)
    현재 방식은 react에 토큰 값을 전달 시켜주기 위해서 url parameter를 활용 하였습니다.
    header를 접근할 방법이 보이지 않아 현 방법을 사용했는데, 테스트 결과 history에도 남지 않고 바로 홈으로 넘겨 주기 때문에 어느정도 문제가 없을 것이라고 생각합니다.
    차후, 해당 토큰을 httpOnly로 보낼 수 있도록 백엔드 서버에 요청하는 방법이 필요하다고 생각합니다.

--- src/main/java/com/sparta/soomtut/util/security/oauth2/OAuth2UserServiceImpl.java
+++ src/main/java/com/sparta/soomtut/util/security/oauth2/OAuth2UserServiceImpl.java
@@ -28,17 +28,15 @@ public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserReques
     
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JwtProvider jwtProvider;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
-        // 동의 및 계속하기를 통해 발급된 {localhost}/login/oauth2/code/{provider}/code=? 를 통해 
+        // 동의 및 계속하기를 통해 발급된 {localhost}/login/oauth2/code/{provider}/code={accesstoken} 를 통해 
         // 발급된 토큰을 security가 해당 url로 온 요청을 캐치하여 OAuth2UserRequest 객체를 생성해서 정보를 담아 보내준다.
         // 해당 토큰을 이용해서 DefaultOAuth2UserService의 loadUser에서 각각의 provider를 통해 유저 정보 요청을 보내준다.
         OAuth2User user = new DefaultOAuth2UserService().loadUser(request);
         
-        String registrationId = request.getClientRegistration()
-                                 .getRegistrationId();
+        String registrationId = request.getClientRegistration().getRegistrationId();
 
         Map<String, Object> attributes = OAuthAttributes.extract(registrationId, user.getAttributes());
 
@@ -64,9 +62,6 @@ public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserReques
                             .build()
                 )
             );
-
-        // access token 발급 위치?
-        String accesstoken = jwtProvider.createToken(member.getEmail(), member.getMemberRole());
 
         return new UserDetailsImpl(member, attributes);
     }
