787b5d0c44f03246f20570dd97b5af73beb07e87
LeeEunseop <xxxnseop@gmail.com>
2023-02-16 21:55:55 +0900

    #55 [Feat]  카테고리 기능 구현

---
+++ src/main/java/com/sparta/soomtut/dto/request/CategoryRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.soomtut.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+public class CategoryRequestDto {
+    private String name;
+}
