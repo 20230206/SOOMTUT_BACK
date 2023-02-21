256522900bb7a21bc85dcada9f52d23873b67619
LeeEunseop <xxxnseop@gmail.com>
2023-02-21 14:22:43 +0900

    [Feat] 수업 확정, 완료, 조회 Repository

--- src/main/java/com/sparta/soomtut/repository/PostRepository.java
+++ src/main/java/com/sparta/soomtut/repository/PostRepository.java
@@ -1,6 +1,11 @@
 package com.sparta.soomtut.repository;
 
 import com.sparta.soomtut.entity.Post;
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -11,5 +16,9 @@ public interface PostRepository extends JpaRepository <Post, Long> {
     Optional<Post>findById(Long postId);
     List<Post> findAllByTutorId(Long tutorId);
     Optional<Post> findByTutorId(Long tutorId);
+    
+    Page<Post> findAll(Pageable pageable);
+    List<Post> findAllById(Long id);
+
 
 }
--- src/main/java/com/sparta/soomtut/repository/TuitionRequestRepository.java
+++ src/main/java/com/sparta/soomtut/repository/TuitionRequestRepository.java
@@ -1,11 +1,16 @@
 package com.sparta.soomtut.repository;
 
 import com.sparta.soomtut.entity.TuitionRequest;
+import com.sparta.soomtut.enums.TuitionState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TuitionRequestRepository extends JpaRepository<TuitionRequest,Long> {
 
     Optional<TuitionRequest> findByPostIdAndTuteeId(Long postId, Long tuteeId);
+    Optional<TuitionRequest> findByPostId(Long postId);
+
+   List<TuitionRequest> findAllByTuteeIdAndTuitionState(Long TuteeId, TuitionState tuitionState);
 }
