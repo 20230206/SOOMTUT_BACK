97a315b79e3e228a687989ce63fd82253bf8954a
Shinny <siniya979@gmail.com>
2023-02-22 18:00:23 +0900

    #65[Feat] : 채팅 메시지에 대한 기능 구현
    1. 가장 최근 채팅 메시지 1개 불러오기 
    2. 모든 대화 내용 리스트로 불러오기 
    기능 구현

---
+++ src/main/java/com/sparta/soomtut/chat/service/ChatService.java
@@ -0,0 +1,16 @@
+package com.sparta.soomtut.chat.service;
+
+import com.sparta.soomtut.chat.dto.ChatRequestDto;
+import com.sparta.soomtut.chat.dto.ChatResponseDto;
+
+import java.util.List;
+
+public interface ChatService {
+
+    void save(ChatRequestDto chatRequest);
+
+    ChatResponseDto getLastChatMessage(Long memberId);
+
+    List<ChatResponseDto> getAllChatMessages(Long memberId);
+
+}
