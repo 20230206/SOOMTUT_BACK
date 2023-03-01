5e7a0edd25961059e7a6ac9c64a0b3d71e13eea8
Shinny <siniya979@gmail.com>
2023-03-02 03:14:36 +0900

    #93[Refactor]: 생성자 수정
    팩토리 패턴으로 수정

---
+++ src/main/java/com/sparta/soomtut/lectureRequest/dto/LecReqResponseDto.java
@@ -0,0 +1,45 @@
+package com.sparta.soomtut.lectureRequest.dto;
+
+import com.sparta.soomtut.lecture.entity.Lecture;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LecReqResponseDto {
+    private Long lectureRequestId;
+    private Lecture lecture;
+    private Long tuteeId;
+    private Boolean reviewFilter;
+
+
+    private LecReqResponseDto(
+            Long lectureRequestId,
+            Lecture lecture,
+            Long tuteeId,
+            Boolean reviewFilter
+    ){
+        this.lectureRequestId = lectureRequestId;
+        this.lecture = lecture;
+        this.tuteeId = tuteeId;
+        this.reviewFilter = reviewFilter;
+    }
+
+    public static LecReqResponseDto of(
+            Long lectureRequestId,
+            Lecture lecture,
+            Long tuteeId,
+            Boolean reviewFilter
+    ){
+        return new LecReqResponseDto(
+                lectureRequestId,
+                lecture,
+                tuteeId,
+                reviewFilter
+        );
+    }
+
+
+}
