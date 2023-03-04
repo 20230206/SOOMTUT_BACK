99eab631991bec27e398a4075df2e403422d1fcf
JungSungYun <kelwho@naver.com>
2023-03-05 00:25:49 +0900

    [Deploy] 리뷰 기능 추가 버전 Release

--- src/main/java/com/sparta/soomtut/config/SecurityConfig.java
+++ src/main/java/com/sparta/soomtut/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // configuration.setAllowedOrigins(Arrays.asList(ENDPOINT_FRONT));
+        configuration.setAllowedOrigins(Arrays.asList(ENDPOINT_FRONT));
         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
